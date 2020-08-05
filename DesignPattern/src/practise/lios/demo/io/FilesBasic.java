57e7e6c6d82bef4fb26d0f4cda80fe91338710c7
李爱光 <aiguang.li@qq.com>
2020-08-05 22:13:28 +0800

    Add file attrbutes get demo

--- DesignPattern/src/practise/lios/demo/io/FilesBasic.java
+++ DesignPattern/src/practise/lios/demo/io/FilesBasic.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 import java.util.Scanner;
 
@@ -37,8 +38,71 @@ public class FilesBasic {
         }
 
         //通过OutputStream写内容到文件
-        Path outPath = Paths.get("output.txt");
-        OutputStream outputStream = Files.newOutputStream(outPath, StandardOpenOption.CREATE);
+        Path outputDirectory = Paths.get("output");
+        //目录不存在的话创建目录
+        if (! Files.exists(outputDirectory)) {
+            Files.createDirectory(outputDirectory);
+        }
+        //将输出文件放入到新加的目录下
+        Path outPath = outputDirectory.resolve("output.txt");
+        boolean fileExists = Files.exists(outPath);
+        OutputStream outputStream;
+        if (fileExists) {
+            outputStream = Files.newOutputStream(outPath, StandardOpenOption.APPEND);
+        } else {
+            outputStream = Files.newOutputStream(outPath, StandardOpenOption.CREATE);
+        }
         outputStream.write(bytes, 0, 100);
+
+        //文件移动、复制及删除
+        Path parentPath = outputDirectory.toAbsolutePath().getParent();
+        if (parentPath != null) {
+            Path destinationFilePath = parentPath.resolve("output.txt");
+            //如果文件存在则替换掉
+            Path copyPath = Files.copy(outPath, destinationFilePath, StandardCopyOption.REPLACE_EXISTING);
+
+            Path movedFilePath = parentPath.resolve("output1.txt");
+            Path movedPath = Files.move(outPath, movedFilePath, StandardCopyOption.REPLACE_EXISTING);
+
+            showFileInfo(movedPath);
+
+            Files.delete(outputDirectory);
+        }
+    }
+
+    public static void showFileInfo(Path path) throws IOException {
+        boolean fileExists = Files.exists(path);
+        System.out.println("File exists: " + fileExists);
+        if (fileExists) {
+            boolean isHidden = Files.isHidden(path);
+            System.out.println("File is hidden: " + isHidden);
+
+            boolean isReadable = Files.isReadable(path);
+            System.out.println("File is readable: " + isReadable);
+
+            boolean isWritable = Files.isWritable(path);
+            System.out.println("File is writable: " + isWritable);
+
+            boolean isExecutable = Files.isExecutable(path);
+            System.out.println("File is executable: " + isExecutable);
+
+            boolean isDirectory = Files.isDirectory(path);
+            System.out.println("File is directory: " + isDirectory);
+
+            boolean isSymbolicLink = Files.isSymbolicLink(path);
+            System.out.println("File is symbolic link: " + isSymbolicLink);
+
+            boolean isRegularFile = Files.isRegularFile(path);
+            if (isRegularFile) {
+                long fileSize = Files.size(path);
+                System.out.printf("File size: %.1fkB \n",(float)fileSize / 1024);
+                //BasicFileAttributes包含上述的全部属性封装
+                BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class);
+                System.out.println("File size: " + attributes.size() + "bytes");
+                System.out.println("File created at: " + attributes.creationTime());
+                System.out.println("File last modified at: " + attributes.lastModifiedTime());
+                System.out.println("File last accessed at: " + attributes.lastAccessTime());
+            }
+        }
     }
 }
