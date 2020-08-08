c23e660912286b2e69a7ec906947b5f759c0582f
李爱光 <aiguang.li@qq.com>
2020-08-08 16:05:55 +0800

    增加正则工具

--- DesignPattern/src/practise/lios/demo/PatternRegexDemo.java
+++ DesignPattern/src/practise/lios/demo/PatternRegexDemo.java
@@ -25,5 +25,43 @@ public class PatternRegexDemo {
             System.out.println("Is Email: " + RegexUtil.isEmail(email));
             System.out.println("Input Email to test: ");
         }
+
+        String url;
+        System.out.println("Input url to test: ");
+        while(! exit.equals((url = scanner.nextLine()).toLowerCase())) {
+            System.out.println("Is url: " + RegexUtil.isUrl(url));
+            System.out.println("Input url to test: ");
+        }
+
+        String asc;
+        System.out.println("Input asc to test: ");
+        while(! exit.equals((asc = scanner.nextLine()).toLowerCase())) {
+            System.out.println("Is asc: " + RegexUtil.isAscString(asc));
+            System.out.println("Input asc to test: ");
+        }
+
+        String integer;
+        System.out.println("Input integer to test: ");
+        while(! exit.equals((integer = scanner.nextLine()).toLowerCase())) {
+            boolean isInteger = RegexUtil.isInteger(integer);
+            System.out.println("Is integer: " + isInteger);
+            if (isInteger) {
+                int value = Integer.parseInt(integer);
+                System.out.println("Integer Value: " + value);
+            }
+            System.out.println("Input integer to test: ");
+        }
+
+        String floatStr;
+        System.out.println("Input Float to test: ");
+        while(! exit.equals((floatStr = scanner.nextLine()).toLowerCase())) {
+            boolean isFloat = RegexUtil.isFloat(floatStr, 1);
+            System.out.println("Is float: " + isFloat);
+            if (isFloat) {
+                float value = Float.parseFloat(floatStr);
+                System.out.println("Float Value: " + value);
+            }
+            System.out.println("Input Float to test: ");
+        }
     }
 }
--- DesignPattern/src/practise/lios/util/RegexUtil.java
+++ DesignPattern/src/practise/lios/util/RegexUtil.java
@@ -10,18 +10,59 @@ import java.util.regex.Pattern;
 public class RegexUtil {
     public static boolean isChinaMobile(String mobile) {
         String regex = "^1[3-9]\\d{9}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(mobile);
 
-        return matcher.matches();
+        return patterMatch(regex, mobile);
     }
 
     public static boolean isEmail(String email) {
         String regex = "^\\w+([._]*\\w)*@\\w+([.\\-_]*\\w)*[.]\\w+$";
 
+        return patterMatch(regex, email);
+    };
+
+    public static boolean isUrl(String url) {
+        String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]$";
+
+        return patterMatch(regex, url.toLowerCase());
+    }
+
+    public static boolean isAscString(String str) {
+        String regex = "^[\\x00-\\x7F]+$";
+
+        return patterMatch(regex, str);
+    }
+
+    public static boolean isInteger(String str) {
+        String regex = "^[-|+]?[0-9]+$";
+
+        return patterMatch(regex, str);
+    }
+
+    /**
+     * 浮点数校验，支持+/-符号，最多6位小数
+     * @param str=要检验的字符
+     * @param pointNum=小数位数
+     * @return boolean
+     */
+    public static boolean isFloat(String str, int pointNum) {
+        final int minPointNum = 1;
+        final int maxPointNum = 6;
+        if (pointNum < minPointNum) {
+            pointNum = minPointNum;
+        }
+        if (pointNum > maxPointNum) {
+            pointNum = maxPointNum;
+        }
+        String regex = String.format("^[-|+]?[0-9]+(.[0-9]{1,%d}){0,1}$", pointNum);
+       // String regex = "^[-|+]?[0-9]+(.[0-9]{1,6}){0,1}$";
+
+        return patterMatch(regex, str);
+    }
+
+    public static boolean patterMatch(String regex, String target) {
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email);
+        Matcher matcher = pattern.matcher(target);
 
         return matcher.matches();
-    };
+    }
 }
